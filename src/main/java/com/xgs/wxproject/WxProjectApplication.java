f9676491a329b821ef915e2fe5a53589db860cb4
xgs <1150840779@qq.com>
2022-04-10 22:17:19 +0800

    Initial commit

---
+++ src/main/java/com/xgs/wxproject/WxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xgs.wxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WxProjectApplication.class, args);
+  }
+
+}
