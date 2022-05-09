b1c50d0749da0b8206ec1af08f20b6da841a8ca6
xgs <1150840779@qq.com>
2022-05-09 21:41:18 +0800

    文章收藏

--- src/main/java/com/xgs/controller/UserController.java
+++ src/main/java/com/xgs/controller/UserController.java
@@ -2,10 +2,12 @@ package com.xgs.controller;
 
 import com.arronlong.httpclientutil.exception.HttpProcessException;
 import com.xgs.dao.UserDao;
+import com.xgs.pojo.Article;
 import com.xgs.pojo.Result;
 import com.xgs.pojo.WechatLoginRequest;
 import com.xgs.pojo.WechatUser;
 import com.xgs.service.impl.UserServiceImpl;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -66,11 +68,67 @@ public class UserController {
   }
 
 
+  //添加收藏
   @RequestMapping(value = "/collectArticle", method = RequestMethod.POST)
-  public String collect(@RequestParam("id") String id, @RequestParam("uid") String uid) {
-    return null;
+  public Result collect(@RequestParam("id") String articleId, @RequestParam("uid") String uid) {
+    //收藏文章
+    WechatUser user = userService.getUser(uid);
+    Result result = new Result();
+    if (user == null) {
+      result.setDesc("请登陆后操作");
+      result.setStatusCode(500);
+      result.setFlag(false);
+      return result;
+    }
+    int userId = user.getId();
+    int artId = Integer.parseInt(articleId);
+    userDao.collect(userId, artId);
+    result.setDesc("收藏成功");
+    result.setFlag(true);
+    result.setStatusCode(200);
+    return result;
   }
 
+  //删除收藏
+  @RequestMapping(value = "/deleteArticle", method = RequestMethod.POST)
+  public Result delete(@RequestParam("id") String articleId, @RequestParam("uid") String uid) {
+    //收藏文章
+    WechatUser user = userService.getUser(uid);
+    Result result = new Result();
+    if (user == null) {
+      result.setDesc("请登陆后操作");
+      result.setStatusCode(500);
+      result.setFlag(false);
+      return result;
+    }
+    int userId = user.getId();
+    int artId = Integer.parseInt(articleId);
+    userDao.delete(userId, artId);
+    result.setDesc("删除成功");
+    result.setFlag(true);
+    result.setStatusCode(200);
+    return result;
+  }
+
+  //展示所有收藏
+  @RequestMapping(value = "/deleteArticle", method = RequestMethod.POST)
+  public Result show(@RequestParam("uid") String uid) {
+    //收藏文章
+    WechatUser user = userService.getUser(uid);
+    Result result = new Result();
+    if (user == null) {
+      result.setDesc("请登陆后操作");
+      result.setStatusCode(500);
+      result.setFlag(false);
+      return result;
+    }
+    List<Article> show = userDao.show(user.getId());
+    result.setData(show);
+    result.setDesc("返回收藏成功");
+    result.setFlag(true);
+    result.setStatusCode(200);
+    return result;
+  }
 
 }
 
--- src/main/java/com/xgs/dao/UserDao.java
+++ src/main/java/com/xgs/dao/UserDao.java
@@ -2,8 +2,10 @@ package com.xgs.dao;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xgs.pojo.Article;
 import com.xgs.pojo.WechatUser;
-import org.apache.ibatis.annotations.Update;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,5 +16,11 @@ public interface UserDao extends BaseMapper<WechatUser> {
   public WechatUser getUserByID(String id);
 
   public int insertUser(WechatUser wechatUser);
+
+  public int collect(@Param("userId") int userId, @Param("articleId") int articleId);
+
+  public int delete(@Param("userId") int userId, @Param("articleId") int articleId);
+
+  public List<Article> show(int userId);
 
 }
